e27ae17f9b4ef0a00ab8fd23a6b3e390e2de4a70
Bertrand Perrier <bertrandperrier@gmail.com>
2013-12-20 11:00:50 +0100

    Create nb2mots.py
    compte de nombre de mots de chaque mot dans un fichier
    triage alpha numérique
    affichage

---
+++ nb2mots.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from operator import itemgetter
+## s = "a e i o a u a y i"
+## source=["bof","bien","super","hyper","mega","bien","super","mega","mega"]
+
+dico={}
+FileHandle = open("text", "r")
+## print ("list source:")
+for ligne in FileHandle:
+	# str to tab
+	tab = ligne.split(' ')
+	## print (str(len(tab))+" mots dans la ligne")
+	for item in tab:
+		## suppression des \n
+		item=item.replace('\n','')
+		## print item 
+		# mot existant ?
+		if dico.has_key(item):
+			dico[item] +=1
+		else:
+			dico[item] = 1
+
+# affichage resultat
+dico_sorted=sorted(dico.iteritems())
+print (str(len(dico_sorted))+" mots comptés")
+print ("list dico:")
+for key,value in dico_sorted: 
+	print(key.encode('utf-8'),value)
+
+
+FileHandle.close()
