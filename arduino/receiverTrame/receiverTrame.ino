6e464072ac1aed1d52396076439eed15ed8aefe4
Bertrand Perrier <bertrandperrier@gmail.com>
2014-01-28 10:54:08 +0100

    merge des bit dans un tableau

--- arduino/receiverTrame/receiverTrame.ino
+++ arduino/receiverTrame/receiverTrame.ino
@@ -1,66 +1,39 @@
 // /dev/ttyUSB0
 #include <LiquidCrystal.h>
-const int pinData = 4; // 2
+const int pinData = 4;
 const int speedDial = 1;
-bool bit0=false;
-bool bit1=false;
-bool bit2=false;
-bool bit3=false;
-bool bit4=false;
-bool bit5=false;
-bool bit6=false;
-bool bit7=false;
+bool trame[] = {0,0,0,0,0,0,0,0};
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 void setup()
   {
-  //attachInterrupt(0, receiveTrame, FALLING);
   lcd.begin(16, 2);
   pinMode(pinData, INPUT);
   lcd.setCursor(0, 0);
   lcd.print("BitOrderMSBFIRST");
+  lcd.setCursor(0, 1);
+  lcd.print("st bit :");
   }
 
 void loop()
   {
   if (digitalRead(pinData))
     {
-    lcd.setCursor(0, 1);
-    lcd.print("st bit :");
-
-    
+    lcd.setCursor(8, 1);
     delay(12*speedDial);
-    bit0=digitalRead(pinData);
-    delay(10*speedDial);
-    bit1=digitalRead(pinData);
-    delay(10*speedDial);
-    bit2=digitalRead(pinData);
-    delay(10*speedDial);
-    bit3=digitalRead(pinData);
-    delay(10*speedDial);
-    bit4=digitalRead(pinData);
-    delay(10*speedDial);
-    bit5=digitalRead(pinData);
-    delay(10*speedDial);
-    bit6=digitalRead(pinData);
-    delay(10*speedDial);
-    bit7=digitalRead(pinData);
     
-    
-    lcd.print(bit0);
-    lcd.print(bit1);
-    lcd.print(bit2);
-    lcd.print(bit3);
-    lcd.print(bit4);
-    lcd.print(bit5);
-    lcd.print(bit6);
-    lcd.print(bit7);
+    for (int num_bit=0;num_bit<=7;num_bit++)
+      {
+      trame[num_bit]=digitalRead(pinData);
+      delay(10*speedDial);
+      }
+
+    for (int num_bit=0;num_bit<=7;num_bit++)
+      {
+        lcd.print(trame[num_bit]);
+      delay(10*speedDial);
+      }
 
     }
-  }
-
-void receiveTrame()
-  {
-
   }
--- arduino/senderTrame/senderTrame.ino
+++ arduino/senderTrame/senderTrame.ino
@@ -1,9 +1,7 @@
 // /dev/ttyACM0
 const int pinData = 8;
-
 const int speedDial = 1;
-
-bool trame[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+bool trame[] = {0,0,0,0,0,0,0,0};
 
 void setup()
   {
@@ -12,24 +10,24 @@ void setup()
 
 void loop()
   {
-for (int val_bin=0;val_bin<=bit(15);val_bin++)  // de 0 à bit(15)
-  {
-  for (int num_bit=0;num_bit<=15;num_bit++) // de 0 à 15 bit
+  for (int val_bin=0;val_bin<=bit(15);val_bin++)  // de 0 à bit(15)
     {
-    trame[7-num_bit] = HIGH && (val_bin & bit(num_bit)); // bit -> 2pn  
+    for (int num_bit=0;num_bit<=15;num_bit++) // de 0 à 15 bit
+      {
+      trame[7-num_bit] = HIGH && (val_bin & bit(num_bit)); // bit -> 2pn  
+      }
+    digitalWrite(pinData, HIGH); // bit depart
+    delay(8*speedDial);
+    digitalWrite(pinData, LOW); // bit depart
+    for (int i=0;i<=7;i++)
+      {
+      digitalWrite(pinData,trame[i]);
+      delay(10*speedDial);
+      }
+    digitalWrite(pinData, LOW); // mise à zéro de la pin data
+    delay(20*speedDial);
+    delay(200); // durée affichage
     }
-  digitalWrite(pinData, HIGH); // bit depart
-  delay(8*speedDial);
-  digitalWrite(pinData, LOW); // bit depart
-  for (int i=0;i<=7;i++)
-    {
-    digitalWrite(pinData,trame[i]);
-    delay(10*speedDial);
-    }
-  digitalWrite(pinData, LOW); // mise à zéro de la pin data
-  delay(20*speedDial);
-  delay(200); // durée affichage
-  }
   delay(1000); // attente avant remise à zero
-}
+  }
     
