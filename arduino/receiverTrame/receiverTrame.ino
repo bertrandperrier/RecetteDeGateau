4c77b4dfa42caac277d9590a8dccc894a3f43ee8
Bertrand Perrier <bertrandperrier@gmail.com>
2014-01-29 18:28:49 +0100

    modif trame sur 10 bit, 1 début 7 carte 2 données

--- arduino/receiverTrame/receiverTrame.ino
+++ arduino/receiverTrame/receiverTrame.ino
@@ -1,8 +1,8 @@
 // /dev/ttyUSB0
 #include <LiquidCrystal.h>
-const int pinData = 4;
-const int speedDial = 1;
-bool trame[] = {0,0,0,0,0,0,0,0};
+#include "/home/bertrand/github/RecetteDeGateau/arduino/libraries/SenderReceiverTrame.h"
+
+
 byte valueReceive=0;
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
@@ -12,42 +12,62 @@ void setup()
   pinMode(pinData, INPUT);
   lcd.setCursor(0, 0);
   //lcd.print("BitOrderMSBFIRST");
-  lcd.print("binaire:");
-  lcd.setCursor(0, 1);
-  //lcd.print("st bit :");
-  lcd.print("decimal:");
+  lcd.print("trame:");
   }
 
 void loop()
   {
+  bool bit_debut = 1; // trame[0]
+  int bits_no_carte = 0; // trame[1-7]
+  int bits_donnees = 0; // trame[8-9]
+  
+  // reception de la tram
   if (digitalRead(pinData))
     {
-    lcd.setCursor(8, 1);
-    delay(12*speedDial);
+    delay(10*speedDial);
     
-    for (int num_bit=0;num_bit<=7;num_bit++)
+    //reception de la trame
+    for (int num_bit=1;num_bit<=9;num_bit++)
       {
       trame[num_bit]=digitalRead(pinData);
       delay(10*speedDial);
       }
-      
-    lcd.setCursor(8, 0);
-    for (int num_bit=0;num_bit<=7;num_bit++)
-      {
-      lcd.print(trame[7-num_bit]);
-      delay(10*speedDial);
-      }
-
     }
     
-  for (int num_bit=0;num_bit<=7;num_bit++)
+  // affichage de la trame
+  lcd.setCursor(6, 0);
+  for (int num_bit=0;num_bit<=9;num_bit++)
+    {
+    lcd.print(trame[num_bit]);
+    }
+  
+  // trame to bits_no_carte
+  for (int num_bit=1;num_bit<=7;num_bit++)
     {
     if (HIGH && trame[num_bit] && bit(num_bit))
       {
-      valueReceive += bit(num_bit);
+      bits_no_carte += bit(7-num_bit);
       }
     }
-    lcd.setCursor(8, 1);
-    lcd.print(valueReceive, 10);
-    valueReceive = 0;
+    
+  // trame to bits_donnees
+  for (int num_bit=8;num_bit<=9;num_bit++)
+    {
+    if (HIGH && trame[num_bit] && bit(num_bit))
+      {
+      bits_donnees += bit(num_bit-8);
+      }
+    }
+
+  lcd.setCursor(0, 1);
+  lcd.print("c:");
+  lcd.print(bits_no_carte, 10);
+  lcd.setCursor(12, 1);
+  lcd.print("d:");
+  lcd.print(bits_donnees, 10);
+  
+  //0123456789012345
+  //trame:1234567890
+  //c:1234567   d:89
+    
   }
--- arduino/senderTrame/senderTrame.ino
+++ arduino/senderTrame/senderTrame.ino
@@ -1,7 +1,5 @@
 // /dev/ttyACM0
-const int pinData = 8;
-const int speedDial = 1;
-bool trame[] = {0,0,0,0,0,0,0,0};
+#include "/home/bertrand/github/RecetteDeGateau/arduino/libraries/SenderReceiverTrame.h"
 
 void setup()
   {
@@ -10,24 +8,45 @@ void setup()
 
 void loop()
   {
+
+  /* demo trame 8 bit de 0 à 11111111
   for (int val_bin=0;val_bin<=bit(15);val_bin++)  // de 0 à 11111111
     {
     for (int num_bit=0;num_bit<=15;num_bit++) // de 0 à 15 bit
       {
       trame[num_bit] = HIGH && (val_bin & bit(num_bit)); // bit -> 2pn  //7-
       }
+  */
+  trame[0] = true; // bit start
+  // no carte
+  trame[1] = false;
+  trame[2] = false;
+  trame[3] = false;
+  trame[4] = true;
+  trame[5] = false;
+  trame[6] = true;
+  trame[7] = true;
+
+  // bits de donnees
+  //trame[8] = true;
+  //trame[9] = true;
+  
+  for (int bits_donnees=0;bits_donnees<=bit(2);bits_donnees++)  // de 00 à 11(3)
+    {
+    trame[8] = HIGH && (bits_donnees & bit(0));
+    trame[9] = HIGH && (bits_donnees & bit(1));
+    
     digitalWrite(pinData, HIGH); // bit depart
     delay(8*speedDial);
     digitalWrite(pinData, LOW); // bit depart
-    for (int i=0;i<=7;i++)
+    for (int i=1;i<=9;i++)
       {
       digitalWrite(pinData,trame[i]);
       delay(10*speedDial);
       }
     digitalWrite(pinData, LOW); // mise à zéro de la pin data
-    delay(20*speedDial);
-    delay(200); // durée affichage
+    delay(700); // durée affichage
     }
-  delay(1000); // attente avant remise à zero
+  delay(100); // attente avant remise à zero
   }
     
