8775ecd7e8c15b2d5cba6ec1df00061d2bb1e06f
Bertrand Perrier <bertrandperrier@gmail.com>
2014-02-06 23:05:15 +0100

    moins d'avertissement à la compilation

--- arduino/LigneDeCommande/test/boucle_for_sans_for/boucle_for_sans_for.cpp
+++ arduino/LigneDeCommande/test/boucle_for_sans_for/boucle_for_sans_for.cpp
@@ -5,7 +5,7 @@
 #include "boucle_for_sans_for.h"
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
-
+int input_pin = 3;
 
 boucle::boucle( int debut=0, int fin=10, bool condition=true)
 {
@@ -29,12 +29,12 @@ int boucle::GetValueBoucle()
 char* boucle::GetStateBoucleTxt()
 {
 	if (this->etat_boucle == 0)
-		return "IDLE    ";
+		return (char*)"IDLE    ";
 	if (this->etat_boucle == 1)
-		return "SENDING ";
+		return (char*)"SENDING ";
 	if (this->etat_boucle == 2)
-		return "FINISHED";
-	return "ERROR   ";
+		return (char*)"FINISHED";
+	return (char*)"ERROR   ";
 }
 
 void boucle::Refresh(bool condition_fin = true)
@@ -44,13 +44,13 @@ void boucle::Refresh(bool condition_fin = true)
 	if (this->etat_boucle == IDLE)
 		this->valeur_boucle=valeur_debut;
 		
-	if (this->etat_boucle == SENDING & this->valeur_boucle<this->valeur_fin)
+	if ((this->etat_boucle == SENDING) & (this->valeur_boucle<this->valeur_fin))
 		this->valeur_boucle++;
 
-	if (this->etat_boucle == SENDING & this->valeur_boucle==this->valeur_fin)
+	if ((this->etat_boucle == SENDING) & (this->valeur_boucle==this->valeur_fin))
 		this->etat_boucle = FINISHED;
 	
-	if (this->etat_boucle == FINISHED & this->condition_fin)
+	if ((this->etat_boucle == FINISHED) & (this->condition_fin))
 		this->etat_boucle = IDLE;
 }
 
@@ -68,9 +68,9 @@ bool boucle::StartBoucle()
 }
 
 
-boucle boucle1(0, 30, false);
+boucle boucle1(0, 20, false);
 boucle boucle2(0, 20, false);
-boucle boucle3(0, 10, false);
+boucle boucle3(0, 20, false);
 long unsigned int millis0;
 long unsigned int millis1;
 
@@ -79,6 +79,7 @@ long unsigned int millis1;
 void setup()
 {
 	lcd.begin(16, 2);
+	pinMode(input_pin, INPUT);
 	millis0=millis();
 	millis1=millis();
 }
@@ -86,15 +87,12 @@ void setup()
 void loop()
 {
 	// routine
-	boucle1.Refresh(true);
-	boucle2.Refresh(true);
-	boucle3.Refresh(boucle1.GetValueBoucle() >= 5); // true=>1:10 false=>2:10
+	boucle1.Refresh(boucle2.GetStateBoucle() == 1); // fin de 1 quand 2 compte
+	boucle2.Refresh(boucle3.GetStateBoucle() == 1); // fin de 2 quand 3 compte
+	boucle3.Refresh(boucle1.GetStateBoucle() == 1); // fin de 3 quand 1 compte
 	
 	
-	if (boucle2.GetValueBoucle() >= 5)
-		boucle3.StartBoucle();
-		
-	if (millis0+5000<millis()) // lancement de la boucle 1 toutes les 5 sec
+	if (millis0+10000<millis()) // toutes les 19 sec, début de 1
 	{
 		if(boucle1.StartBoucle())
 		{
@@ -102,7 +100,7 @@ void loop()
 		}
 	}
 	
-	if (millis1+2000<millis()) // lancement de la boucle 2 toutes les 2 sec
+	if (boucle1.GetStateBoucle() == FINISHED) // quand 1 fini, début de 2
 	{
 		if(boucle2.StartBoucle())
 		{
@@ -110,19 +108,22 @@ void loop()
 		}
 	}
 	
+	if (boucle2.GetStateBoucle() == FINISHED) // quand input_pin, début de 3 
+		boucle3.StartBoucle();
+		
 	lcd.setCursor(0, 0); // 1ere ligne
 	lcd.print(boucle1.GetStateBoucle());
 	lcd.print(":");
 	lcd.print(boucle1.GetValueBoucle());
 	lcd.print(" ");
-	lcd.print(boucle3.GetStateBoucle());
-	lcd.print(":");
-	lcd.print(boucle3.GetValueBoucle());
-	lcd.print("   ");
-	lcd.setCursor(0, 1); // 2eme ligne
 	lcd.print(boucle2.GetStateBoucle());
 	lcd.print(":");
 	lcd.print(boucle2.GetValueBoucle());
 	lcd.print(" ");
+	lcd.print(boucle3.GetStateBoucle());
+	lcd.print(":");
+	lcd.print(boucle3.GetValueBoucle());
+	lcd.print(" ");
+	lcd.setCursor(0, 1); // 2eme ligne
 	delay(100);
 }
