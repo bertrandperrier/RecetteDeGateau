cba48aa2cf2873dff466208fef4efaf65a90357c
Bertrand Perrier <bertrandperrier@gmail.com>
2014-02-11 21:07:56 +0100

    rangement des déclarations

--- arduino/LigneDeCommande/test/boucle_for_sans_for/boucle_for_sans_for.h
+++ arduino/LigneDeCommande/test/boucle_for_sans_for/boucle_for_sans_for.h
@@ -1,24 +1,23 @@
-#ifndef MACLASSE_H
-#define MACLASSE_H
-
 typedef enum { IDLE, SENDING, FINISHED } boucle_state;
 
 class boucle
 {
-public:
+private:
 	int valeur_boucle;
 	int valeur_debut;
 	int valeur_fin;
 	bool condition_fin;
 	volatile boucle_state etat_boucle;
+	
+public:
 	boucle(int, int, bool);
+	
 	boucle_state GetStateBoucle();
 	int GetValueBoucle();
+	char* GetStateBoucleTxt();
+	
 	void Refresh(bool );
 	bool StartBoucle();
-	char* GetStateBoucleTxt();
 };
- 
-#endif
 
 
