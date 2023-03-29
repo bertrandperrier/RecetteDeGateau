ed688c29dc72210f0e9adc5873ec0521dedbe972
Bertrand Perrier <bertrandperrier@gmail.com>
2023-03-29 20:47:36 +0200

    modification suite à changement de version  python

--- python/gravity.py
+++ python/gravity.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 import sys
 
-s = raw_input('Saissisez le nombre de lignes et de colonnes [L C] ou [demo]-->')
+s = input('Saissisez le nombre de lignes et de colonnes [L C] ou [demo]-->')
 if s=="demo":
 	nb_colonne = 7
 	nb_ligne = 9
@@ -12,30 +12,30 @@ else:
 		nb_ligne=int(s[0])
 		nb_colonne=int(s[1])
 	except:
-		print "erreur saisie : nombres non détectés"
+		print("erreur saisie : nombres non détectés")
 		sys.exit(1)
 	
 	if len(s)> 2:
-		print "erreur saisie : plus de deux chiffres saisies"
+		print("erreur saisie : plus de deux chiffres saisies")
 		sys.exit(1)
 	
 	colonne=[]
 	for i in range(nb_ligne):
 		while True:
-			print "ligne n°"+str(i)
+			print("ligne n°"+str(i))
 			temp=(raw_input('-->'))
 			temp_s=temp.split()
 			if len(temp)==nb_colonne:
 				colonne.extend(temp_s)
 				break
-			print "erreur : "+str(nb_colonne)+" colonnes demandées"
+			print("erreur : "+str(nb_colonne)+" colonnes demandées")
 	
-print ""
-print "origine"
-print "-------"
-print ""
+print("")
+print("origine")
+print("-------")
+print("")
 for i in range(len(colonne)):
-	print colonne[i]
+	print(colonne[i])
 
 colonne_m= [[0 for x in range(nb_colonne)] for y in range(nb_ligne)]
 
@@ -49,9 +49,9 @@ for temp in range(nb_ligne): # scan de la grille nb_ligne fois
 			if colonne_m[i][j] == ".":
 				colonne_m[i][j]   = colonne_m[i-1][j]
 				colonne_m[i-1][j] = "."
-print ""
-print "modifié"
-print "-------"
-print ""
+print("")
+print("modifié")
+print("-------")
+print("")
 for i in range(nb_ligne):
-		print ''.join(colonne_m[i])
+		print(''.join(colonne_m[i]))
