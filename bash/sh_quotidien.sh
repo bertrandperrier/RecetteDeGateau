50e47f09887d88150631a7e06ee1e67460be1fb9
bertrandperrier <bertrandperrier@laposte.net>
2023-04-09 19:04:55 +0200

    maj des commande apt au lieu d'apt-get

--- bash/sh_quotidien.sh
+++ bash/sh_quotidien.sh
@@ -5,8 +5,8 @@ sudo ls /var/spool/exim4/input >> /home/$USER/linux/script/liste_id_email ## jou
 message=$(ls -lh /home/$USER/linux/script/liste_id_email | cut -d " " -f5)
 if [ $message -eq 0 ]
 then
-	sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoremove && apt list --upgradable
+	sudo apt -qq update && sudo apt -qq upgrade && sudo apt -qq dist-upgrade && sudo apt -qq autoremove && apt -qq list --upgradable
 	sh /home/$USER/linux/script/grep_updatedns.sh
 	sh /home/$USER/linux/script/voir_jour.sh
-	geany /home/$USER/Documents/a\ faire.txt
+ 	#gedit /home/$USER/Documents/a\ faire.txt
 fi
