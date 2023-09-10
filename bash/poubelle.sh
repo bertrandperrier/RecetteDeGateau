24beecbce93bc7a746e9ff332ae31bcf034f1a26
bertrandperrier <bertrandperrier@laposte.net>
2023-09-10 19:12:49 +0200

    ajout d'une comfirmation avant poubelle

--- bash/poubelle.sh
+++ bash/poubelle.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
-##if [ $1 = * ]
-##	then
-##		echo "etoile"
-##	else
-##		echo $1
-##fi
-##mv * ~/.local/share/Trash/files/
-mv $1 ~/.local/share/Trash/files/
+if [ -f "$1" ]
+then
+	read -p "mettre à la poubelle ? o/N" reponse
+	if [ "$reponse" = "o" ]
+	then
+		mv $1 /home/$USER/.local/share/Trash/files/
+		echo $1
+		echo "mis à la poubelle"
+	else
+		echo "annulé"
+	fi
+else
+	echo "le fichier n'existe pas"
+fi
