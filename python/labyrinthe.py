b294c08428129e9412bf82ae356b4a9ec68eb681
bertrandperrier <bertrandperrier@laposte.net>
2024-08-08 22:20:09 +0200

    maj mineur

--- python/labyrinthe.py
+++ python/labyrinthe.py
@@ -3,13 +3,13 @@
 import os
 import sys
 import termios
+import time
 
 verbose = 0
 tabl_of_point_cardinal_labyrinthe = []
-colonne, ligne = 6,7
 tab_emplacement = [0, 0]
-tab_gagne = [7, 6]
-
+dir_prec_demandee = ""
+nb_depl = 0
 def autor_depl(tab_emplacement, dir_demandee):
 	# si la direction demandée n'est pas un mur
 	for point_cardinal in tabl_of_point_cardinal_labyrinthe[tab_emplacement[0]][tab_emplacement[1]]:
@@ -50,13 +50,40 @@ def readchar() -> str:
 
 # construction de la table du labyrinthe
 tabl_of_point_cardinal_labyrinthe = [['s',  'se',   'eo', 'so',   'se',  'seo', 'so'],
-			    ['ns',  'ns',  'se', 'no',   'ns',  'ns',  'ns'],
-			    ['ns',  'ns',  'ns', 'se',   'nso', 'ns',  'ns'],
-			    ['ns',  'ns',  'ns', 'ns',   'ns',  'ns',  'ns'],
-			    ['nse', 'nso', 'ns', 'ns',   'ns',  'ns',  'ns'],
-			    ['ns',  'ns',  'n',  'ns',   'n',   'ns',  'ns'],
-			    ['ns',  'ne',  'eo', 'nseo', 'eo',  'no',  'ns'],
-			    ['ne',  'eo',  'eo', 'neo',  'eo',  'o',   'n']]
+				['ns',  'ns',  'se', 'no',   'ns',  'ns',  'ns'],
+				['ns',  'ns',  'ns', 'se',   'nso', 'ns',  'ns'],
+				['ns',  'ns',  'ns', 'ns',   'ns',  'ns',  'ns'],
+				['nse', 'nso', 'ns', 'ns',   'ns',  'ns',  'ns'],
+				['ns',  'ns',  'n',  'ns',   'n',   'ns',  'ns'],
+				['ns',  'ne',  'eo', 'nseo', 'eo',  'no',  'ns'],
+				['ne',  'eo',  'eo', 'neo',  'eo',  'o',   'n']]
+
+colonne, ligne = 6,7
+tab_gagne = [7, 6]
+dir_demandee = "s"
+"""
+tabl_of_point_cardinal_labyrinthe = [['e',  'eo',   'eo', 'eo',   'eo',  'eo', 'so'],
+					['ns',  'ns',  'se', 'no',   'ns',  'ns',  'ns'],
+					['ns',  'ns',  'ns', 'se',   'nso', 'ns',  'ns'],
+					['ns',  'ns',  'ns', 'ns',   'ns',  'ns',  'ns'],
+					['nse', 'nso', 'ns', 'ns',   'ns',  'ns',  'ns'],
+					['ns',  'ns',  'n',  'ns',   'n',   'ns',  'ns'],
+					['ns',  'ne',  'eo', 'nseo', 'eo',  'no',  'ns'],
+					['ne',  'eo',  'eo', 'neo',  'eo',  'o',   'n']]
+
+
+tabl_of_point_cardinal_labyrinthe =    [['e',  'eo',   'seo',  'eo',   'seo',  'eo',  'so'],
+					['se',  'o',   'ns',   's',    'ns',   's',   'ns'],
+					['ne',  'eo',  'nso',  'ns',   'ns',   'ns',  'ns'],
+					['se',  'so',  'ns',   'ne',   'no',   'ne',  'nso'],
+					['ns',  'ns',  'nse',  'seo',  'eo',   'eo',  'nso'],
+					['ns',  'ns',  'ns',   'ns',   's',    'se',  'nso'],
+					['ns',  'n',   'ns',   'ns',   'ne',   'no',  'ns'],
+					['ne',  'eo',  'no',   'ne',   'eo',   'eo',  'no']]
+"""
+#y, x de 0 à 
+tab_gagne = [7, 6]
+dir_demandee = "o"
 
 if len(sys.argv) > 1:
 	if sys.argv[1] == "-v":
@@ -74,6 +101,30 @@ print("|   | | | | | |")
 print("| | |_| |_| | |")
 print("| |____  ___| |")
 print("|___________|x|")
+
+dessin=""
+print("__________________")
+for i in range(ligne):
+		for j in range(colonne):
+			match tabl_of_point_cardinal_labyrinthe[i][j]:
+				case "n":	dessin+="|_|"
+				case "s":	dessin+="| |"
+				case "e":	dessin+="|= "
+				case "o":	dessin+=" =|"
+				case "se":	dessin+="|  "
+				case "eo":	dessin+=" = "
+				case "so":	dessin+="  |"
+				case "ns":	dessin+="| |"
+				case "no":	dessin+=" _|"
+				case "ne":	dessin+="|_ xxx"
+				case "nso":	dessin+="  |"
+				case "nse":	dessin+="|  "
+				case "seo":	dessin+="   "
+				case "neo":	dessin+=" _ "
+				case "nseo":	dessin+="   "
+		print(dessin)
+		dessin = ""
+print("------------------")
 print("\nDirections\n z\nq+d\n s\n\nx pour quitter\n")
 
 if verbose:
@@ -83,6 +134,15 @@ if verbose:
 			print("L:"+str(i)+" C:"+str(j)+" "+str(tabl_of_point_cardinal_labyrinthe[i][j]))
 
 input("Entrer pour commencer")
+
+mode = input("mode Manuel ou Automatique (M/a)")
+
+if (mode == "A"):
+	mode = "a"
+else:
+	mode =  "m"
+
+
 while(1):
 	# effacement de l'écran
 	os.system('clear')
@@ -113,26 +173,98 @@ while(1):
 	# check sortie du labyrinthe
 	if (tab_emplacement[0] == tab_gagne[0] and tab_emplacement[1] == tab_gagne[1]):
 		print("gagné")
+		print("nb_depl : "+str(nb_depl))
 		quit()
+	
+	if (mode == "m"):
+		#saisie de la direction
+		while True:
+			key = readchar()
+			if key == "z":
+				dir_demandee="n"
+				break
+			if key == "q":
+				dir_demandee="o"
+				break
+			if key == "d":
+				dir_demandee="e"
+				break
+			if key == "s":
+				dir_demandee="s"
+				break
+			if key == "x":
+				quit()
+
 		
-	# saisie de la direction
-	while True:
-		key = readchar()
-		if key == "z":
-			dir_demandee="n"
-			break
-		if key == "q":
-			dir_demandee="o"
-			break
-		if key == "d":
-			dir_demandee="e"
-			break
-		if key == "s":
-			dir_demandee="s"
-			break
-		if key == "x":
-			quit()
+	
+		
+	if (mode == "a"):
+		match tabl_of_point_cardinal_labyrinthe[tab_emplacement[0]][tab_emplacement[1]]:
+			case "n":	key = "z"
+			case "s":	key = "s"
+			case "e":	key = "d"
+			case "o":	key = "q"
+			case "se":
+				if (dir_prec_demandee=="n"): key = "d"
+				if (dir_prec_demandee=="o"): key = "s"		
+			case "eo":
+				if (dir_prec_demandee=="e"): key = "d"
+				if (dir_prec_demandee=="o"): key = "q"
+			case "so":
+				if (dir_prec_demandee=="n"): key = "q"
+				if (dir_prec_demandee=="e"): key = "s"
+			case "ns":
+				if (dir_prec_demandee=="n"): key = "z"
+				if (dir_prec_demandee=="s"): key = "s"
+			case "no":
+				if (dir_prec_demandee=="s"): key = "q"
+				if (dir_prec_demandee=="e"): key = "z"
+			case "ne":
+				if (dir_prec_demandee=="s"): key = "d"
+				if (dir_prec_demandee=="o"): key = "z"
+			case "nso":
+				if (dir_prec_demandee=="n"): key = "q"
+				if (dir_prec_demandee=="s"): key = "z"
+				if (dir_prec_demandee=="e"): key = "s"
+			case "nse":
+				if (dir_prec_demandee=="n"): key = "s"
+				if (dir_prec_demandee=="s"): key = "d"
+				if (dir_prec_demandee=="o"): key = "z"
+			case "seo":
+				if (dir_prec_demandee=="n"): key = "d"
+				if (dir_prec_demandee=="e"): key = "d"
+				if (dir_prec_demandee=="o"): key = "q"
+			case "neo":
+				if (dir_prec_demandee=="s"): key = "q"
+				if (dir_prec_demandee=="e"): key = "d"
+				if (dir_prec_demandee=="o"): key = "n"
+			case "nseo":
+				if (dir_prec_demandee=="n"): key = "q"
+				if (dir_prec_demandee=="s"): key = "d"
+				if (dir_prec_demandee=="e"): key = "z"
+				if (dir_prec_demandee=="o"): key = "s"
+		
+	print("dir : "+key)
+	print("dir_demandee : "+dir_demandee)
+	print("dir_prec_demandee : "+dir_prec_demandee)
+	print("nb_depl : "+str(nb_depl))
+	
+	if (mode == "a"):
+		time.sleep(0.5)
+	
+	if key == "z":
+		dir_demandee="n"
+	if key == "q":
+		dir_demandee="o"
+	if key == "d":
+		dir_demandee="e"
+	if key == "s":
+		dir_demandee="s"
+	if key == "x":
+		quit()
+	dir_prec_demandee=dir_demandee
 
 	# si déplacement autorisé -> déplacement demandé
 	if (autor_depl(tab_emplacement, dir_demandee)):
-		tab_emplacement=depl(tab_emplacement, dir_demandee)	
+		tab_emplacement=depl(tab_emplacement, dir_demandee)
+		nb_depl=nb_depl+1
