c60df8d21120e7bca11587a908b0bd6b8b001f20
bertrandperrier <bertrandperrier@laposte.net>
2025-01-06 20:19:49 +0100

    maj de sécurité

--- bash/poubelle.sh
+++ bash/poubelle.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-if [ -f "$1" ]
+if [ $USER != "root" ]
 then
-	read -p "mettre à la poubelle ? o/N" reponse
-	if [ "$reponse" = "o" ]
+	if [ -e $1 ]
 	then
-		mv $1 /home/$USER/.local/share/Trash/files/
-		echo $1
-		echo "mis à la poubelle"
+		read -p "mettre à la poubelle (o/N) ?" reponse
+		if [ $reponse = "o" ]
+		then
+			mv $1 /home/$USER/.local/share/Trash/files/
+			echo "${1} mis à la poubelle"
+		else
+			echo "annulé"
+		fi
 	else
-		echo "annulé"
+		echo "le fichier n'existe pas"
 	fi
 else
-	echo "le fichier n'existe pas"
+	echo "ne pas utiliser en root"
 fi
