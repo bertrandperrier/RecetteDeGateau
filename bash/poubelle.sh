567e77d54018f72f027349fbf983d5073777393c
bertrandperrier <bertrandperrier@laposte.net>
2026-05-03 19:12:02 +0200

    poubelle.sh accepte les noms de fichiers contenants des espaces

--- bash/poubelle.sh
+++ bash/poubelle.sh
@@ -2,12 +2,12 @@
 # à copier dans /usr/local/bin/
 if [ $USER != "root" ]
 then
-	if [ -e $1 ]
+	if [ -e "$1" ]
 	then
 		read -p "mettre à la poubelle (o/N) ?" reponse
 		if [ $reponse = "o" ]
 		then
-			mv $1 /home/$USER/.local/share/Trash/files/
+			mv "$1" "/home/$USER/.local/share/Trash/files/"
 			echo "${1} mis à la poubelle"
 		else
 			echo "annulé"
